8c9c00afe1fac4bb438c58aba7c991c45c8db499
Adocrafter <107572581+Adocrafter@users.noreply.github.com>
2023-05-07 10:46:04 +0200

    Fixed an error where  intents are required.
    In newer versions (2023) when creating a bot in order to send a message to the server, user is required to specify which intents to give him for bot to be able to send messages, menage channels, etc. With this fix, after going into the discord developer portal and granting the bot message content intent, bot will be able to send messages to the server with this fix.

--- bot.py
+++ bot.py
@@ -14,7 +14,10 @@ async def send_message(message, user_message, is_private):
 
 def run_discord_bot():
     TOKEN = 'YOUR_KEY'
-    client = discord.Client()
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.messages = True
+    client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
